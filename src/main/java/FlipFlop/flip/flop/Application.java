19f237b6270a1aa47ce00ece4ee809186dad1cfd
wujizhu1999 <wujizhu1999@outlook.com>
2022-03-26 21:33:15 +1100

    change back

--- src/main/java/FlipFlop/flip/flop/Application.java
+++ src/main/java/FlipFlop/flip/flop/Application.java
@@ -8,14 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/helloo")
+	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
