a29a754e64b83affa974b2e9e0d3bbb6adc8b2b0
wujizhu1999 <wujizhu1999@outlook.com>
2022-03-26 22:43:44 +1100

    change back, add static folder

--- src/main/java/FlipFlop/flip/flop/Application.java
+++ src/main/java/FlipFlop/flip/flop/Application.java
@@ -13,10 +13,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
-
 }
