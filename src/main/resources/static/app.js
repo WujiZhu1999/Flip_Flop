993bb241cc2744616d1e10567f305a4ee79872f5
wujizhu1999 <wujizhu1999@outlook.com>
2022-03-27 19:10:55 +1100

    communication socket enabled

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,5 @@
 var stompClient = null;
 
-
 function setConnected(connected) {
     $("#connect").prop("disabled", connected);
     $("#disconnect").prop("disabled", !connected);
@@ -41,6 +40,28 @@ function clicking() {
     stompClient.send("/app/click", {}, JSON.stringify({'w': parseInt($("#w").val()),'h': parseInt($("#h").val())}));
 }
 
+var ffClient = null;
+
+function ffConnect() {
+    var socket = new SockJS('/ff2-socket');
+    ffClient = Stomp.over(socket);
+    ffClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        ffClient.subscribe('/ffGame/room1', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+function ffStartBoard() {
+    ffClient.send("/ffApp/start", {}, JSON.stringify({'name': $("#name").val()}));
+}
+
+function ffFlipBoard() {
+    ffClient.send("/ffApp/flip", {}, JSON.stringify({'w': parseInt($("#w").val()),'h': parseInt($("#h").val())}));
+}
+
 function showGreeting(message) {
     $("#greetings").append("<tr><td>" + message + "</td></tr>");
 }
@@ -53,4 +74,14 @@ $(function () {
     $( "#disconnect" ).click(function() { disconnect(); });
     $( "#send" ).click(function() { sendName(); });
     $( "#click").click(function (){clicking(); });
+    $( "#ffStart").click(function () {
+        ffStartBoard();
+    });
+
+    $( "#ffFlip").click(function () {
+        ffFlipBoard();
+    });
+    $( "#ffConnect").click(function (){
+        ffConnect();
+    });
 });
