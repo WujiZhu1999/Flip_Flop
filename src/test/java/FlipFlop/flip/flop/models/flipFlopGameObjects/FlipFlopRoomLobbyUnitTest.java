49959dcb52b85ad64aaf4eff9b502d6ec6b852fd
wujizhu1999 <wujizhu1999@outlook.com>
2022-04-12 00:00:09 +1000

    Test for Room Lobby, test for game objects done

--- src/test/java/FlipFlop/flip/flop/models/flipFlopGameObjects/FlipFlopRoomLobbyUnitTest.java
+++ src/test/java/FlipFlop/flip/flop/models/flipFlopGameObjects/FlipFlopRoomLobbyUnitTest.java
@@ -1,4 +1,33 @@
 package FlipFlop.flip.flop.models.flipFlopGameObjects;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
 public class FlipFlopRoomLobbyUnitTest {
+
+    @Test
+    void testAddRoom(){
+        String roomKey = FlipFlopRoomLobby.getInstance().addRoom(new FlipFlopRoom());
+
+        assertNotNull(FlipFlopRoomLobby.getInstance().getRoom(roomKey));
+    }
+
+    @Test
+    void testGetRoomNotExist(){
+        String roomKey = FlipFlopRoomLobby.getInstance().addRoom(new FlipFlopRoom()) + "abcde";
+
+        assertNull(FlipFlopRoomLobby.getInstance().getRoom(roomKey));
+    }
+
+    @Test
+    void testRemoveExistRoom(){
+        String roomKey = FlipFlopRoomLobby.getInstance().addRoom(new FlipFlopRoom());
+        FlipFlopRoomLobby.getInstance().removeRoom(FlipFlopRoomLobby.getInstance().getRoom(roomKey));
+
+        assertNull(FlipFlopRoomLobby.getInstance().getRoom(roomKey));
+    }
 }
